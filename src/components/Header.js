2e86f7466998cf88f69433d5e126103c762281b8
Vamshi Krishna <vamsikrishna2644@gmail.com>
2023-05-06 11:40:04 +0530

    installed styled componets

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,35 @@
 import React from "react";
+import styled from "styled-components";
+import { Pinterest } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <Wrapper>
+      <LogoWrapper>
+        <IconButton>
+          <Pinterest />
+        </IconButton>
+      </LogoWrapper>
+    </Wrapper>
+  );
 }
 
 export default Header;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  height: 56px;
+  padding: 12px 4px 4px 16px;
+  background-color: white;
+  color: black;
+`;
+
+const LogoWrapper = styled.div`
+  .MuiSvgIcon-root {
+    color: #e60023;
+    font-size: 32px;
+    cursor: pointer;
+  }
+`;
